c17796594a8ca5170cbf2a97c588c299f466dc7a
jim-low <robil12380@gmail.com>
2020-10-27 18:50:17 +0800

    added node server

---
+++ server.js
@@ -0,0 +1,8 @@
+const readline = require('readline')
+const rl = readline.createInterface({input: process.stdin, output: process.stdout})
+
+rl.question("What's 9 + 10? ", (usrInput) => {
+	if(usrInput.trim() === "21") console.log("you stoopid");
+	else console.log("actually idk, lmao");
+	rl.close()
+})
