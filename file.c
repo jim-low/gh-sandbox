49f242d3bf0554f7cc835dd07aef5eb028c5897d
jim-low <robil12380@gmail.com>
2020-12-21 00:53:26 +0800

    added 3 comments for testing

--- file.c
+++ file.c
@@ -2,6 +2,11 @@
 
 int main() {
 	int n = 10;
+	// this comment is not important
+	
+	// this comment is most definitely important
+	
+	// this comment is sh!t
 	for(int i = 0; i < n; i++){
 		printf("hello world\n");
 	}
