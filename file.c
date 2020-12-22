ae935314763df7de8f73810b848d4f7c705b3873
jim-low <robil12380@gmail.com>
2020-12-22 20:36:51 +0800

    made changes, i guess??

--- file.c
+++ file.c
@@ -2,9 +2,6 @@
 
 int main() {
 	int n = 10;
-	// this comment is extremely important (or not lmao, jk...unless?) (coming from master branch)
-	
-	// this comment is most definitely important (coming from test branch)
 	for(int i = 0; i < n; i++){
 		printf("hello world\n");
 	}
