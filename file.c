20612237643ef808d151c556638441e6bcfce161
jim-low <robil12380@gmail.com>
2021-01-03 19:56:03 +0800

    foo again lmao

--- file.c
+++ file.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// helo world, this comment is undoubtedly and completely useless
+// helo world, this comment is undoubtedly and completely, utterly useless
 
 int main() {
 	printf("you suck");
