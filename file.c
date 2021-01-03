b45bad9f45df2f1f4668edb5a5790e34de54e1e0
jim-low <robil12380@gmail.com>
2021-01-03 20:16:56 +0800

    another foo message lmao

--- file.c
+++ file.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-// hello world from branch 'test'
+// hello world, this comment is very important indeed
+//
+//
+// hello world, this comment is not important
+//
+//
+// hello world, this comment is up to you to decide
 
 int main() {
 	printf("you suck");
