c280032281673bca7965dbbec46c5fb4c4c99af7
jim-low <robil12380@gmail.com>
2021-04-22 11:13:23 +0800

    i have no idea what the heck got changed but oh well

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const readline = require('readline')
 const rl = readline.createInterface({input: process.stdin, output: process.stdout})
 
 rl.question("What's 9 + 10? ", (usrInput) => {
-	if(usrInput.trim() === "21") console.log("you stoopid");
-	else console.log("why u ask me? lmao");
-	rl.close()
+    if(usrInput.trim() === "21") console.log("you stoopid");
+    else console.log("why u ask me? lmao");
+    rl.close()
 })
