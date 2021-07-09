44e310835b9e0bf67adba95fcb5c4f0ef62e5a5f
diongzy <yuii.yuii265@gmail.com>
2021-07-09 18:36:24 +0800

    ziyu's fourth change
    printf("Fourth change made by ziyu hiiiiiiiii");

--- file.c
+++ file.c
@@ -10,6 +10,7 @@ void languageServerTest(int param1, char param2, char* param3) {
 int main() {
     languageServerTest(69, 'A', "hello world");
     printf("Third change made by ziyu hiiiiiiiii");
+    printf("Fourth change made by ziyu hiiiiiiiii");
     some change into here
     return 0;
 }
