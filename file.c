6af144d02b0236a2075d4b3a6ab6e80d0a6d76b8
weiSan02 <86460890+weiSan02@users.noreply.github.com>
2021-07-13 17:06:28 +0800

    Feature1
    Feature1..

--- file.c
+++ file.c
@@ -9,6 +9,7 @@ int main() {
     printf("Enter a number: \n");
     scanf("%d", &y);
     printf("You entered %d!\n", x);
+    printf("This is feature 1");
 
     printf("Third change made by ziyu hiiiiiiiii");
     printf("Fourth change made by ziyu hiiiiiiiii");
